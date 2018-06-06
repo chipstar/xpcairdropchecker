f96799da58fd0b721d09c51d07a65611698d78fa
chipstar <oobayashi@gmail.com>
2018-06-07 08:17:05 +0900

    add reaction

--- bot.rb
+++ bot.rb
@@ -10,15 +10,17 @@ bot.command [:ping], channels: ["bot_control"] do |event|
 end
 
 bot.command [:airdrop, :エアドロップ, :エアドロ, :ad] do |event, addr|
-  # event.message.create_reaction("hourglass_flowing_sand")
+  event.message.react "\u23f3"
   a = Mechanize.new
   begin
     r = a.get("https://insight-b.xpjp.online/api/addr/#{addr}/balance")
     j = JSON.parse(r.body)
     j = j.to_f
     j = j * 0.00000001
+    event.message.react "\u2705"
     event.send_message "#{event.user.mention} **Xp-QtウォレットかCCWalletのアドレスなら**\nたぶん `#{j} XPC` 受け取れるよ。\n楽しみに待っててね。\n取引所やPoSプールのアドレスだと受け取れないよ。ごめんね。"
   rescue
+    event.message.react "\u274c"
     event.send_message "#{event.user.mention} アドレスを正しく指定してね"
   end
 end
